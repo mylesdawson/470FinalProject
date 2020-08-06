e42f4754147c39027af7626011e85a77ed0aa0f6
Carter Kelly <carterk@sfu.ca>
2020-08-05 22:34:30 -0700

    update admin.py

--- backend/skedge/booking/admin.py
+++ backend/skedge/booking/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Customer, Business, Employee, Service, Appointment
+from .models import Customer, Business, Service, Appointment, Favorites, Employee, Client
 
-# Register your models here.
 admin.site.register(Customer)
 admin.site.register(Business)
-admin.site.register(Employee)
 admin.site.register(Service)
 admin.site.register(Appointment)
+admin.site.register(Favorites)
+admin.site.register(Employee)
+admin.site.register(Client)
