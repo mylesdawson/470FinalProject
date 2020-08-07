f677cc1c6e9bc719fbf8497c67827d35e60fa765
Mihai Lapuste <mlapuste@sfu.ca>
2020-08-07 13:55:14 -0700

    services front end is modified

--- frontend/src/api/services.js
+++ frontend/src/api/services.js
@@ -2,12 +2,63 @@ export default {
     data () {
       return {
         services: [
-          { service: 'Full Service', description: "Haircut, Beard trim or hot shave, and style", price: "35", duration: "40" },
-          { service: 'Fade', description: "Fade haircut", price: "25", duration: "30" },
-          { service: 'Beard Trim', description: "Beard trim and styling service", price: "15", duration: "20" },
-          { service: 'Hair Style', description: "Hair styling service", price: "10", duration: "10" },
+          { name: 'Full Service', description: "Haircut, Beard trim or hot shave, and style", price: 35, duration: 40 },
+          { name: 'Fade', description: "Fade haircut", price: 25, duration: 30 },
+          { name: 'Beard Trim', description: "Beard trim and styling service", price: 15, duration: 20 },
+          { name: 'Hair Style', description: "Hair styling service", price: 10, duration: 10 },
         ]
       }
     }
   }
+
+
+// export async function getServices() {
+  
+//   const headers = new Headers()
+//   headers.append("Content-Type", "application/json")
+
+//   const options = {
+//     method,
+//     headers
+//   };
+
+//   return fetch(host + url, options)
+//     .then(resp => resp.json())
+//     .then(res => {
+//       // console.log(res)
+//       return res
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+
+// }
+
+export async function editService(name, description, price, duration) {
+  const body = {
+    name: name,
+    description: description,
+    price: price,
+    duration: duration
+  }
+
+  const res = await request(`/services/`, body, 'PUT')
+  console.log(res)
+  return res
+}
+
+export async function createService(name, description, price, duration) {
+  const body = {
+    name: name,
+    description: description,
+    price: price,
+    duration: duration
+  }
+
+  const res = await request(`/services/`, body, 'POST')
+  console.log(res)
+  return res
+}
+
+
   
--- frontend/src/components/ServicesPage.vue
+++ frontend/src/components/ServicesPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <b-container fluid>
-    <b-row>
+<div>
+
 <!-- FIRST COLUMN (SERVICES TABLE) -->
-        <b-col cols="12" lg="8"> 
+        
             <b-card>
               <b-row>
                     <b-col cols="12" md="3">
@@ -17,7 +17,7 @@
                     </b-input-group>
                   </b-col>
                   <b-col cols="3" md="3" style="text-align: right">
-                  <b-button >
+                  <b-button v-on:click="showCreate">
                     Add Service
                   </b-button>
                 </b-col>
@@ -25,66 +25,179 @@
 
               <br>
               
-              <b-table striped hover :items="services"></b-table>
+              <b-table 
+                    responsive 
+                    flex 
+                    striped 
+                    hover
+                    :items="services.services"
+                    @row-clicked="showEdit"
+                    ></b-table>
             </b-card>
-        </b-col>
+    
 
-<!-- SECOND COLUMN (SERVICE) -->
-    <b-col cols="12" lg="4">
-            <b-card
-      title=  "Service">
-      <b-form>
+
+
+<template>
+    <modal name="serviceCreate"
+          :width="800"
+          :height="800"
+          :adaptive="true" >
+           <b-card no-body style="border: none">
+            <b-card-header>
+            <b-row>
+                    <b-col >
+                    <h3>New service</h3>
+                    </b-col>
+                 
+              </b-row>
+            </b-card-header>
+            <b-card-body>
+           
+             <b-form>
         <b-form-group
           id="input-group-1"
-          label="Title: "
+          label="Name"
           label-for="input-1">
           <b-form-input
             id="input-1"
-            type="email"
+            type="text"
             required
-            placeholder="Title"></b-form-input>
+            v-model="newService.name"
+            placeholder="Service name"></b-form-input>
         </b-form-group>
 
         <b-form-group
           id="input-group-1"
-          label="Description: "
+          label="Description"
           label-for="input-1">
           <b-form-textarea
             id="input-1"
-            type="email"
+            type="text"
             required
-            placeholder="Description of listing"></b-form-textarea>
+            v-model="newService.description"
+            placeholder="Service description"></b-form-textarea>
         </b-form-group>
 
 
         <b-form-group
           id="input-group-1"
-          label="Services: "
+          label="Price $"
           label-for="input-1">
-          <b-form-input
-            id="input-1"
-            type="email"
-            required></b-form-input>
+         
+          <b-form-spinbutton
+              min="0"
+              max="100000"
+              step="0.25"
+              v-model="newService.price">
+            </b-form-spinbutton>
+           
         </b-form-group>
 
         <b-form-group
+       
           id="input-group-1"
-          label="Members: "
+          label="Duration (minutes)"
           label-for="input-1">
-          <b-form-input
-            id="input-1"
-            type="email"
-            required></b-form-input>
+             <b-form-spinbutton
+              wrap
+              min="0"
+              max="180"
+              step="5"
+              v-model="newService.duration">
+            </b-form-spinbutton>
         </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <b-button type="submit" variant="outline">cancel</b-button>
+        <b-button type="submit" variant="primary" v-on:click="createService">Save</b-button>
+        <b-button variant="outline" v-on:click="cancelCreation">Cancel</b-button>
 
       </b-form>
-       </b-card>
-    </b-col>
-  </b-row>
-</b-container>
+        </b-card-body>
+      </b-card>
+    </modal>
+</template>
+
+
+<template>
+    <modal name="serviceEdit"
+          :width="800"
+          :height="800"
+          :adaptive="true" >
+           <b-card no-body style="border: none">
+            <b-card-header>
+            <b-row>
+                    <b-col >
+                    <h3>{{selectedItem.service}}</h3>
+                    </b-col>
+                 
+                  <b-col style="text-align: right">
+                    <b-button variant="outline-danger" v-on:click="deleteService">
+                      Delete
+                    </b-button>
+                </b-col>
+              </b-row>
+            </b-card-header>
+            <b-card-body>
+           
+             <b-form>
+        <b-form-group
+          id="input-group-1"
+          label="Name"
+          label-for="input-1">
+          <b-form-input
+            id="input-1"
+            type="text"
+            required
+            placeholder="Service name"></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-1"
+          label="Description"
+          label-for="input-1">
+          <b-form-textarea
+            id="input-1"
+            type="text"
+            required
+            placeholder="Service description"></b-form-textarea>
+        </b-form-group>
+
+
+        <b-form-group
+          id="input-group-1"
+          label="Price $"
+          label-for="input-1">
+         
+          <b-form-spinbutton
+              min="0"
+              max="100000"
+              step="0.25">
+            </b-form-spinbutton>
+           
+        </b-form-group>
+
+        <b-form-group
+       
+          id="input-group-1"
+          label="Duration (minutes)"
+          label-for="input-1">
+             <b-form-spinbutton
+              wrap
+              min="0"
+              max="180"
+              step="5">
+            </b-form-spinbutton>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Save</b-button>
+        <b-button variant="outline" v-on:click="hideAll">Cancel</b-button>
+
+      </b-form>
+        </b-card-body>
+      </b-card>
+    </modal>
+</template>
+</div>
 
 </template>
 
@@ -94,8 +207,56 @@
  import services from '../api/services.js'
   export default {
    
-    data () {
-      return services.data();
+     data: function() {
+      return {
+         selectedItem: {},
+         newService: {
+           name: '',
+           description: '',
+           price: 0,
+           duration: 0,
+         },
+         services: services.data(),
+      } 
+    },
+    mounted() {
+      
+    },
+    methods: {
+      showEdit: function(item, index) {
+          this.selectedItem = item;
+          console.log(item, index);
+          this.$modal.show('serviceEdit');
+      },
+      showCreate: function() {
+          this.$modal.show('serviceCreate');
+      },
+      hideAll: function() {
+          this.$modal.hide('serviceCreate');
+          this.$modal.hide('serviceEdit');
+      },
+      createService: function() {
+          console.log(this.newService)
+      },
+      cancelCreation: function() {
+          var result = confirm("Are you sure cancel creating this service?");
+          if (result) {
+              this.newService = {
+                name: '',
+                description: '',
+                price: 0,
+                duration: 0,
+              };
+              this.hideAll();
+          }
+      },
+      deleteService: function() {
+          var result = confirm("Are you sure you want to delete this service?");
+          if (result) {
+              // delete logic
+              this.hideAll();
+          }
+      }
       
     }
   
