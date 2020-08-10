b0b0b4c29a50d32a16756afde3a502773ae49300
Carter Kelly <carterk@sfu.ca>
2020-08-09 18:43:37 -0700

    servicecomponent.vue & data.json changes

--- frontend/src/components/ServiceComponent.vue
+++ frontend/src/components/ServiceComponent.vue
@@ -1,16 +1,19 @@
 <template>
   <b-list-group-item>
     <b-row align-v="center">
-      <b-col cols="8">
+      <b-col cols="2">
         <strong>{{ service.name }}</strong>
         <br>
-        Duration {{ service.duration }}
+        {{ service.duration }} minutes
+      </b-col>
+      <b-col cols="6">
+        {{ service.description }}
       </b-col>
       <b-col cols="2" style="font-size: 1.5rem;">
         <strong>$ {{ service.price }}</strong>
       </b-col>
       <b-col cols="2">
-        <b-button>Book</b-button>
+        <b-button>Book Appointment</b-button>
       </b-col>
     </b-row>
   </b-list-group-item>
