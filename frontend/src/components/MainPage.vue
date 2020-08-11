1304c1809316a9e5ead9474a2f7196c80005ae66
Trevor Sclater <tsclater@sfu.ca>
2020-08-10 19:08:20 -0700

    UI for home page

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,17 +1,25 @@
 <template>
   <b-container fluid>
-    <h1 class="text-center"><strong>Explore</strong> and <strong>book</strong> anything <strong>fitness</strong>, <strong>wellness</strong>, <strong>beauty</strong></h1>
+
+    <p style="font-size: 60px; text-align: center;"><strong>Skedge</strong></p>
+
+    <b-row align-h="center" class="mb-5">
+      <img center src="../assets/logo.png" width="200px" height="200px" class="center">
+    </b-row>
+
+    <br>
+
+    <p style="font-size: 30px; text-align: center;"><strong>Explore</strong> and <strong>book</strong> anything <strong>fitness</strong>, <strong>wellness</strong>, <strong>beauty</strong></p>
 
     <!-- <search-component></search-component> -->
 
-    <h2 class="text-center">
-      <br><strong>Explore</strong> Skedge
-    </h2>
     <br>
-    <b-row align-h="center">
-      <b-button class="mr-5" size="lg" name="fitness" variant="primary" @click="searchWithProps">Fitness</b-button>
-      <b-button class="mr-5" size="lg" name="wellness" variant="success" @click="searchWithProps">Wellness</b-button>
-      <b-button class="mr-5" size="lg" name="beauty" variant="warning" @click="searchWithProps">Beauty</b-button>
+    <br>
+
+    <b-row align-h="center" class="mb-5">
+      <b-button class="mr-5" size="lg" name="fitness" style="background-color: #0091EA; color: black; width: 16rem; height: 140px; border-radius: 30px; font-size: 40px; font-weight:bold; border: none; margin-bottom: 20px;" @click="searchWithProps">Fitness</b-button>
+      <b-button class="mr-5" size="lg" name="wellness" style="background-color: #00E676; color: black; width: 16rem; height: 140px; border-radius: 30px; font-size: 40px; font-weight:bold; border: none; margin-bottom: 20px" @click="searchWithProps">Wellness</b-button>
+      <b-button class="mr-5" size="lg" name="beauty" style="background-color: #FF9E80; color: black; width: 16rem; height: 140px; border-radius: 30px; font-size: 40px; font-weight:bold; border: none" @click="searchWithProps">Beauty</b-button>
     </b-row>
 
   </b-container>
