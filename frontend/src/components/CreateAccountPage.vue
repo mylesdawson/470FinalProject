900769a30b422df24397cfa8486371956795e2f3
= <=>
2020-08-10 23:19:14 -0700

    Fixed phone validation

--- frontend/src/components/CreateAccountPage.vue
+++ frontend/src/components/CreateAccountPage.vue
@@ -40,6 +40,7 @@
               v-model="form.customer.phone_number"
               required
               type="tel"
+              pattern="\(?[0-9]{3}\)?-?[0-9]{3}-?[0-9]{4}"
               placeholder="Enter Phone Number"
             ></b-form-input>
           </b-form-group>
